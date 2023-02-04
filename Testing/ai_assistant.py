2e9c4a36a672086507e1f1afdf78078d47f11cff
botirk38 <btrghstk@gmail.com>
2023-02-04 16:23:56 +0000

    put sounds]

--- Testing/ai_assistant.py
+++ Testing/ai_assistant.py
@@ -81,24 +81,41 @@ class rick_atsley:
         # then return a rick atsley lyric if the input starts with a word in his songs
         # otherwise return the input
         if audio.startswith("Never"):
+            playsound('never_gonna_give_you_up.mp3')
             return "Gonna give you up."
+
         if audio.startswith("Gonna"):
             # randomly choose one of the lyrics
             # return the lyric
             lyrics = ["Gonna let you down", "Gonna run around and desert you", "Gonna make you cry", "Gonna say goodbye", "Gonna tell a lie and hurt you"]
+            if random.choice[lyrics] == "Gonna let you down":
+                playsound('never_gonna_let_you_down.mp3')
+            elif random.choice[lyrics] == "Gonna run around and desert you":
+                playsound('never_gonna_run_around_and_desert_you.mp3')
+            elif random.choice[lyrics] == "Gonna make you cry":
+                playsound('never_gonna_make_you_cry.mp3')
+            elif random.choice[lyrics] == "Gonna say goodbye":
+                playsound('never_gonna_say_goodbye.mp3')
+            elif random.choice[lyrics] == "Gonna tell a lie and hurt you":
+                playsound('never_gonna_tell_a_lie_and_hurt_you.mp3')
             return random.choice(lyrics)
 
 
         elif audio.startswith("What"):
-            return "Inside, we both know what's been going on (going on)"
+            playsound('what_do_you_want_from_me.mp3')
+            return "What do you want from me?"
         if audio.startsWith("Inside"):
+            playsound('inside_we_both_know_what_has_been_going_on.mp3')
             return "Inside, we both know what's been going on (going on)"
 
         elif audio.startswith("You"):
+            playsound('you_know_the_rules_and_so_do_i.mp3')
             return "You know what the rules are."
         elif audio.startswith("I"):
+            playsound('i_just_wanna_tell_you_how_im_feeling.mp3')
             return "I just wanna tell you how I'm feeling."
         elif audio.startswith("A"):
+            playsound('a_full_commitment_thats_what_im_thinking_of.mp3')
             return "A full commitment that's what I'm thinking of"
     
     def get_time():
@@ -141,4 +158,37 @@ class rick_atsley:
         speak(search.text)
         search.click()
         speak(f"Opening {app}")
+
+def main():
+    # Create the AI assistant
+    # Then run the AI assistant
+    # The AI assistant will listen for input
+    # If the input is a command, then the AI assistant will run the command
+    # If the input is not a command, then the AI assistant will process the input
+    # The AI assistant will then speak the processed input
+    # The AI assistant will then listen for input again
+    # This will continue until the AI assistant is terminated
+    # The AI assistant is terminated by saying "goodbye"
+    # The AI assistant will then say "goodbye" and terminate
+    rick = rick_atsley()
+    speak("Hello, I am Rick Astley. How can I help you?")
+    while True:
+        query = listen().lower()
+        if "time" in query:
+            get_time()
+        elif "date" in query:
+            get_date()
+        elif "day" in query:
+            get_day()
+        elif "weather" in query:
+            get_weather()
+        elif "news" in query:
+            get_news()
+        elif "open" in query:
+            get_app()
+        elif "goodbye" in query:
+            speak("Goodbye")
+            break
+        else:
+            speak(process(query))
 
